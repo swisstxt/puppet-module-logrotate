a8716fca073a2eb03b38d4f899d2255d4e70a59a
Marcel Haerry <haerry@puzzle.ch>
2008-05-06 15:38:24 +0200

    init module

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,21 @@
 
 
 # modules_dir { "logrotate": }
-class logrotate {}
+class logrotate {
+    include logrotate::base
+}
+class logrotate::base {
+    package{logrotate:
+        ensure => present,
+    }
+
+    file{"/etc/cron.daily/logrotate":
+        source => [ "puppet://$server/logrotate/logrotate.cron.daily.${operatingsystem}.${lsbdistrelease}",
+                    "puppet://$server/logrotate/logrotate.cron.daily.${operatingsystem}",
+                    "puppet://$server/logrotate/logrotate.cron.daily" ],
+        require => Package[logrotate],
+        owner => root, group => 0, mode => 0755;
+    }
+}
+
+
