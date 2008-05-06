bb14ce166ade5c567119e4e978b4140b67c89002
Marcel Haerry <haerry@puzzle.ch>
2008-05-06 15:25:14 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# logrotate module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "logrotate": }
+class logrotate {}
